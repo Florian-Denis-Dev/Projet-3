c3b4d3c6b156647227622a605ca31a02b1a002a3
florian denis <136095872+Azherak@users.noreply.github.com>
2023-10-24 16:09:46 +0200

    ajout back end + front end

---
+++ FrontEnd/projets.js
@@ -0,0 +1,36 @@
+// Sélection de la galerie dans le DOM
+let gallery = document.querySelector('.gallery');
+
+// Suppression du contenu HTML existant dans la galerie
+gallery.innerHTML = '';
+
+// Fonction pour récupérer les données de l'API
+async function getData() {
+  const response = await fetch('http://localhost:5678/api/works'); // url de l'api
+  const data = await response.json();
+  return data;
+}
+
+// Fonction pour ajouter les images et descriptions à index.html
+function addToHTML(data) {
+  data.forEach(item => {
+      // Créer un nouvel élément figure
+      const figure = document.createElement('figure');
+
+      // Ajouter une image à la figure
+      const img = document.createElement('img');
+      img.src = item.imageUrl; // 
+      figure.appendChild(img);
+
+      // Ajouter une description à la figure
+      const description = document.createElement('figcaption');
+      description.textContent = item.title; // 
+      figure.appendChild(description);
+
+      // Ajouter la figure au conteneur gallery
+      gallery.appendChild(figure);
+  });
+}
+
+// Appeler la fonction getData et utiliser les données reçues pour appeler addToHTML
+// getData().then(data => addToHTML(data));
