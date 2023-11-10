12e9af2f21c7e5b0a9bca799e8b64fee55fa0de7
florian denis <136095872+Azherak@users.noreply.github.com>
2023-11-10 09:56:28 +0100

    test ajout photos js
    - code fonctionnel en js pour l'ajout
    - fix a venir: display des éléments au click sur logout ; gestion du formulaire en html?

--- FrontEnd/islogged.js
+++ FrontEnd/islogged.js
@@ -1,20 +1,20 @@
 function checkToken() {
-    const token = window.localStorage.getItem("token");
-    if (token) {
-      const loginLink = document.querySelector('.login-link');
-      loginLink.textContent = 'Logout';
-      loginLink.href = 'login.html'; // 
-  
-      // suppression du token dans le localstorage lors du click
-      loginLink.addEventListener('click', function() {
-        window.localStorage.removeItem("token");
-        const edition = document.querySelectorAll('.edition');
-        edition.forEach((element) => {
-            element.style.display = 'none';
-        }); 
-      });
-    }
+  const token = window.localStorage.getItem("token");
+  if (token) {
+    const loginLink = document.querySelector('.login-link');
+    loginLink.textContent = 'Logout';
+    loginLink.href = 'login.html'; // 
+
+    // suppression du token dans le localstorage lors du click
+    loginLink.addEventListener('click', function() {
+      window.localStorage.removeItem("token");
+      const edition = document.querySelectorAll('.edition');
+      edition.forEach((element) => {
+          element.style.display = 'none';
+      }); 
+    });
   }
-  
-  // Appelez la fonction checkToken lorsque la page est chargée
-  window.addEventListener('DOMContentLoaded', checkToken);
+}
+
+// Appelez la fonction checkToken lorsque la page est chargée
+window.addEventListener('DOMContentLoaded', checkToken);
