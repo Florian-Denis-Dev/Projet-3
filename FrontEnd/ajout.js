df6917651b7c18094828e9a77fa072db1e0ba699
florian denis <136095872+Azherak@users.noreply.github.com>
2023-11-16 16:11:21 +0100

    import img + categorie
    - fix mineur style, html
    - ajout js pour les différentes catégories
    - ajout js ajout d'image

---
+++ FrontEnd/ajout.js
@@ -0,0 +1,42 @@
+// Fonction pour récupérer les données de l'API
+async function getCategories() {
+    const response = await fetch('http://localhost:5678/api/categories'); // url de l'api
+    const data = await response.json();
+    return data;
+  }
+  
+  // Fonction pour ajouter les catégories au select
+  function addToSelect(data) {
+    // Sélectionnez le select dans le DOM
+    const select = document.querySelector('#categorie');
+  
+    data.forEach(item => {
+      // Créez une nouvelle option
+      const option = document.createElement('option');
+      option.value = item.id;
+      option.text = item.name;
+  
+      // Ajoutez l'option au select
+      select.add(option);
+    });
+  }
+  
+  // Appeler la fonction getCategories et utiliser les données reçues pour appeler addToSelect
+  getCategories().then(data => addToSelect(data));
+
+  
+  /** function de click sur ajouter photo */
+
+  document.getElementById('addButton').addEventListener('click', function() {
+    document.getElementById('imageUpload').click();
+  });
+  
+  document.getElementById('imageUpload').addEventListener('change', function() {
+    const file = this.files[0];
+    if (file) {
+      // Faites quelque chose avec le fichier, par exemple l'envoyer à votre serveur
+      console.log(file);
+    } else {
+      console.log('Aucun fichier sélectionné');
+    }
+  });
