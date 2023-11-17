03166cbd2bccffe429327b81d097e796e562544b
florian denis <136095872+Azherak@users.noreply.github.com>
2023-11-17 11:01:22 +0100

    test

--- FrontEnd/import.js
+++ FrontEnd/import.js
@@ -3,11 +3,14 @@ let gallery = document.querySelector('.gallery')
 // Sélection du bouton dans le DOM
 const validateButton = document.getElementById('valider');
 const dialog = document.getElementById('dialog');
-
+const token = window.localStorage.getItem("token");
+const form = document.querySelector('.ajout-text');
 // Ajoutez un écouteur d'événements au bouton
-validateButton.addEventListener('click', async function() {
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
   const fileInput = document.getElementById('imageUpload');
   const file = fileInput.files[0];
+  alert("test");
   if (file) {
     // Créez un FormData pour stocker le fichier
     const formData = new FormData();
@@ -22,8 +25,9 @@ validateButton.addEventListener('click', async function() {
     // Envoyez une requête POST à votre API avec le fichier
     fetch('http://localhost:5678/api/works', {
       method: 'POST',
-      body: formData
-    })
+      body: formData,
+      headers: {'Authorization': `Bearer ${token}`}
+      })
     .then(response => response.json())
     .then(data => {
       console.log('Image uploaded successfully:', data);
@@ -32,10 +36,10 @@ validateButton.addEventListener('click', async function() {
       dialog.style.display = 'none';
       
       // Supprimez les données de votre galerie
-      gallery.innerHTML = '';
+      //gallery.innerHTML = '';
 
       // Appelez votre fonction addToHTML
-      addToHTML(data);
+      //addToHTML(data);
     })
     .catch(error => {
       console.error('Error:', error);
