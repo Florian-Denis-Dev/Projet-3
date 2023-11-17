ae4d19af74857d6beaaaecc3235b745a7780d662
florian denis <136095872+Azherak@users.noreply.github.com>
2023-11-17 10:26:30 +0100

    pre-fix import
    - bug post
    - futur fix fct addtohtml

--- FrontEnd/import.js
+++ FrontEnd/import.js
@@ -1,38 +1,46 @@
-  // Sélectionnez le bouton dans le DOM
-  const validateButton = document.getElementById('valider');
-  const dialog = document.getElementById('dialog');
+// Sélection de gallery dans le DOM
+let gallery = document.querySelector('.gallery')
+// Sélection du bouton dans le DOM
+const validateButton = document.getElementById('valider');
+const dialog = document.getElementById('dialog');
 
-  // Ajoutez un écouteur d'événements au bouton
-  validateButton.addEventListener('click', function() {
-    const fileInput = document.getElementById('imageUpload');
-    const file = fileInput.files[0];
-    if (file) {
-      // Créez un FormData pour stocker le fichier
-      const formData = new FormData();
-      formData.append('image', file);
-  
-      // Envoyez une requête POST à votre API avec le fichier
-      fetch('http://localhost:5678/api/works', {
-        method: 'POST',
-        body: formData
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Image uploaded successfully:', data);
-  
-        // Fermez la pop-up
-        dialog.style.display('none');
-  
-        // Supprimez les données de votre galerie
-        gallery.innerHTML = '';
-  
-        // Appelez votre fonction addToHTML
-        addToHTML(data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-    } else {
-      console.log('Aucun fichier sélectionné');
-    }
-  });
+// Ajoutez un écouteur d'événements au bouton
+validateButton.addEventListener('click', async function() {
+  const fileInput = document.getElementById('imageUpload');
+  const file = fileInput.files[0];
+  if (file) {
+    // Créez un FormData pour stocker le fichier
+    const formData = new FormData();
+    formData.append('image', file);
+
+    // Ajoutez le nom du champ et la catégorie au formData
+    const fieldName = document.getElementById('titre').value;
+    const category = document.getElementById('categorie').value;
+    formData.append('fieldName', fieldName);
+    formData.append('category', category);
+
+    // Envoyez une requête POST à votre API avec le fichier
+    fetch('http://localhost:5678/api/works', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Image uploaded successfully:', data);
+
+      // Fermez la pop-up
+      dialog.style.display = 'none';
+      
+      // Supprimez les données de votre galerie
+      gallery.innerHTML = '';
+
+      // Appelez votre fonction addToHTML
+      addToHTML(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  } else {
+    console.log('Aucun fichier sélectionné');
+  }
+});
